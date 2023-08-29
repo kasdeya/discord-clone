de210758e1a305b866c40ecd1ad12f471b06d76c
kas <kasdeyaa@hotmail.com>
2023-08-29 15:47:17 -0700

    working on form to create a server.

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { db } from '@/lib/db';
 import { initialProfile } from '@/lib/initial-profile';
+import { InitialModal } from '@/components/modals/initial-modal';
 
 const SetupPage = async () => {
   // initialize profile...
@@ -24,7 +25,7 @@ const SetupPage = async () => {
   }
 
   // else
-  return <div>create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
