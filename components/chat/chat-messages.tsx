7034fab485b8d29459d7330d02effd32cb063d70
kas <kasdeyaa@hotmail.com>
2023-09-03 10:20:23 -0700

    Chat socket hook added, now it updates and works in real time, had to downgrade to next 13.4.12

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,11 @@ import { ChatWelcome } from './chat-welcome';
 import { useChatQuery } from '@/hooks/use-chat-query';
 import { Loader2, ServerCrash } from 'lucide-react';
 import { Fragment } from 'react';
+import { ChatItem } from '@/components/chat/chat-item';
+import { format } from 'date-fns';
+import { useChatSocket } from '@/hooks/use-chat-socket';
+
+const DATE_FORMAT = 'd MMM yyyy, HH:mm';
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -36,6 +41,8 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
@@ -44,6 +51,7 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
+  useChatSocket({ queryKey, addKey, updateKey });
 
   if (status === 'loading') {
     return (
@@ -75,7 +83,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, index) => (
           <Fragment key={index}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                currentMember={member}
+                key={message.id}
+                id={message.id}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
